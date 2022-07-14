93292917aeae450d62763ad6268ae4350de7e9c9
Admin <abdutokt2004>
2022-07-14 13:54:01 +0600

    HW39

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         carHashMap.put(1, audi);
         carHashMap.put(2, mercedes);
         carHashMap.put(3, lexus);
-        for (Map.Entry<Integer, Car> entry: carHashMap.entrySet()) {
+        for (Map.Entry<Integer, Car> entry : carHashMap.entrySet()) {
             System.out.println(entry);
         }
     }
